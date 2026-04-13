aab153e2c00a0397c7e3cf90f6373ffa7e2c3f7b
christian-nieves <thechristiannieves@gmail.com>
2026-04-13 13:49:45 -0400

    Finished with ifs & capital first letters

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -9,18 +9,40 @@ public class FullNameApplication {
         System.out.println("Please enter your name");
 
         System.out.println("First name: "); // Asking user for first name
-        String firstName = myScanner.nextLine(); // Storing first name
+        String firstName = myScanner.nextLine().trim(); // Storing first name
+
+        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
 
         System.out.println("Middle name: "); // Asking user for middle name
-        String middleName = myScanner.nextLine(); // Storing middle name
+        String middleName = myScanner.nextLine().trim(); // Storing middle name
+
+        middleName = middleName.substring(0, 1).toUpperCase() + middleName.substring(1);
 
         System.out.println("Last name: "); // Asking user for last name
-        String lastName = myScanner.nextLine(); // Storing last name
+        String lastName = myScanner.nextLine().trim(); // Storing last name
+
+        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
 
         System.out.println("Suffix: "); // Asking user for suffix
-        String suffix = myScanner.next(); // Storing suffix
+        String suffix = myScanner.nextLine().trim(); // Storing suffix
 
-        System.out.println(firstName + " " + middleName + " " + lastName + ", " + suffix);
+        suffix = suffix.substring(0, 1).toUpperCase() + suffix.substring(1);
+
+        String fullName = (firstName + " " + middleName + " " + lastName + ", " + suffix); // Making a variable for the full name
+
+        if (middleName.isEmpty() && suffix.isEmpty()) {
+            fullName = (firstName + " " + lastName); // Removing middle and suffix if left blank
+        }
+        if (middleName.isEmpty()) {
+            fullName = (firstName + " " + lastName + ", " + suffix); // Removing middle name if left blank
+        }
+        if (suffix.isEmpty()) {
+            fullName = (firstName + " " + middleName + " " + lastName); // Removing suffix if left blank
+        }
+
+        System.out.println("Full name: " + fullName);
+
+
 
 
 
