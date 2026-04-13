025b7922fa1aae33b9d8139de5da29ca7aa42c0e
christian-nieves <thechristiannieves@gmail.com>
2026-04-13 13:55:55 -0400

    Final commit. Finished and polished everything.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -11,22 +11,22 @@ public class FullNameApplication {
         System.out.println("First name: "); // Asking user for first name
         String firstName = myScanner.nextLine().trim(); // Storing first name
 
-        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1); // Capitalizes the first letter of first name
 
         System.out.println("Middle name: "); // Asking user for middle name
         String middleName = myScanner.nextLine().trim(); // Storing middle name
 
-        middleName = middleName.substring(0, 1).toUpperCase() + middleName.substring(1);
+        middleName = middleName.substring(0, 1).toUpperCase() + middleName.substring(1); // Capitalizes the first letter of middle name
 
         System.out.println("Last name: "); // Asking user for last name
         String lastName = myScanner.nextLine().trim(); // Storing last name
 
-        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1); // Capitalizes the first letter of last name
 
         System.out.println("Suffix: "); // Asking user for suffix
         String suffix = myScanner.nextLine().trim(); // Storing suffix
 
-        suffix = suffix.substring(0, 1).toUpperCase() + suffix.substring(1);
+        suffix = suffix.substring(0, 1).toUpperCase() + suffix.substring(1); // Capitalizes the first letter of suffix
 
         String fullName = (firstName + " " + middleName + " " + lastName + ", " + suffix); // Making a variable for the full name
 
@@ -40,6 +40,8 @@ public class FullNameApplication {
             fullName = (firstName + " " + middleName + " " + lastName); // Removing suffix if left blank
         }
 
+        System.out.println(" ");
+
         System.out.println("Full name: " + fullName);
 
 
@@ -49,4 +51,5 @@ public class FullNameApplication {
 
 
     }
+
 }
